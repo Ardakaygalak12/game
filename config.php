2a54ff3d1206d233e4d3ff438c9846100aeaec50
Ardakaygalak12 <ardakaygalak6@gmail.com>
2025-02-12 23:18:24 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('FAUCETPAY_API_KEY', 'a5a15c7cc4b4d4c6a0cab0cb5d2b2ce3388070028e2c5cf37284f627975cb9ed');
+define('MIN_CLAIM', 0.00000100); // Minimum ödeme miktarı
+define('MAX_CLAIM', 0.00000300); // Maximum ödeme miktarı
+?>
