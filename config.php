87ea307f129dbaacf06ac6022096e63fd77a5ef4
Ardakaygalak12 <ardakaygalak6@gmail.com>
2025-02-14 20:13:52 +0300

    Delete config.php

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('FAUCETPAY_API_KEY', 'a5a15c7cc4b4d4c6a0cab0cb5d2b2ce3388070028e2c5cf37284f627975cb9ed');
-define('MIN_CLAIM', 0.01); // Minimum ödeme miktarı DGB
-define('MAX_CLAIM', 0.03); // Maximum ödeme miktarı DGB
-?>
